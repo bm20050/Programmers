ca3e0f96bb115ef10a31d1207f6f47719123cabb
bm20050 <bm20050@gmail.com>
2023-02-19 22:51:01 +0900

    02.19 update

--- Lv.1/완주하지 못한 선수.py
+++ Lv.1/완주하지 못한 선수.py
@@ -1,3 +1,5 @@
+# 완주하지 못한 선수
+
 def solution(participant, completion):
     participant.sort()
     completion.sort()
@@ -7,6 +9,7 @@ def solution(participant, completion):
             return participant[i]
 
     return participant[-1]
+
 
 participant = ["leo", "kiki", "eden"]
 completion = ["eden", "kiki"]
