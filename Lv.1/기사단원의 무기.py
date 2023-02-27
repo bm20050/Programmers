f690877ba700ce76509002119607cb8666310b03
bm20050 <bm20050@gmail.com>
2023-02-27 18:04:47 +0900

    02.27 update

--- Lv.1/기사단원의 무기.py
+++ Lv.1/기사단원의 무기.py
@@ -1,7 +1,17 @@
 def solution(number, limit, power):
-    array = []
-
+    sum = 0
     for i in range(1, number + 1):
+        count = 0
+        for j in range(1, int(i ** (1 / 2)) + 1):
+            if i % j == 0:
+                count += 1
+                if j ** 2 != i:
+                    count += 1
+        if count <= limit:
+            sum += count
+        else:
+            sum += power
+    return sum
 
 
 number = 5
@@ -9,3 +19,4 @@ limit = 3
 power = 2
 
 print(solution(number, limit, power))
+
